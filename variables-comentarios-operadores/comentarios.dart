4911906207f1b816238c1fc342ede644fec43a3d
crisdux <crisfer.4217@gmail.com>
2023-09-14 01:18:29 -0400

    comentarios

---
+++ variables-comentarios-operadores/comentarios.dart
@@ -0,0 +1,18 @@
+main(){
+  var a = 10; // este es un comentario de una sola linea
+
+  /*
+    Es es un comentario
+    multilinea  en dart
+  */
+
+  /**
+   * 
+   * sumar tiene dos parametros: [a] y [b]
+   * 
+  */
+
+  sumar(int a, int b){
+    // los comentarios de este formato para la funcion sumar, son utiles para auto dicumentar codigo con otros programas
+  }
+}
