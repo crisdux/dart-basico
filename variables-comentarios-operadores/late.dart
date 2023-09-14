bc7d61a2b38349d04964f958d2775c48505ee818
crisdux <crisfer.4217@gmail.com>
2023-09-14 01:08:50 -0400

    se agrego late

---
+++ variables-comentarios-operadores/late.dart
@@ -0,0 +1,10 @@
+main(){
+  String nombre;
+  nombre = "Cris";
+  print(nombre);
+
+  late String apellido; // no inicializamos la variable, pero con late nos aseguramos que cuando la variable sea usada tendra un valor
+  apellido = "Villca";
+  print(apellido);
+
+}
