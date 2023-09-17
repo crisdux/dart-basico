9ef19cac978b9573a019990c404532a22e66fd08
crisdux <crisfer.4217@gmail.com>
2023-09-16 23:38:03 -0400

    arrow functions en dart

---
+++ 04-funciones/05_funciones-flecha.dart
@@ -0,0 +1,22 @@
+main(){
+  int sumar(int a, int b){
+    return a + b;
+  }
+
+  int respuesta = sumar(5, 6);
+  print(respuesta);
+
+  int sumar2(int a, int b) => a + b;
+  print(sumar2(6, 1));
+
+  String saludar(String nombre) => "Hola $nombre";
+  print(saludar("Cris"));
+
+  List<int> numeros = [1,2,3,4,5,6,7];
+  List<int> salida = numeros.where((item) => item>4 && item<6).toList();
+  print(salida);
+
+  // dado el siguiente arreglo, filtrarlo sin que hayan nombres repetidos y entregar el resultado en un arreglo
+  List<String> nombres = ["Carlos", "Juan", "Pedro", "Juan", "Roberto", "Carlos"];
+  print(nombres.toSet().toList());
+}
